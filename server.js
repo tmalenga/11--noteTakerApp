8379f0f6544604befb9e9000051894c0e1af3ca6
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-04-08 12:37:47 -0300

    Update server to incl path

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require ("express");
 const bodyParser = require("body-parser");
 const apiRoutes = require("./routes/notes");
-const path = require("path");
+const htmlRoutes = require("./routes/html");
+// const path = require("path");
 
 const PORT = process.env.port || 3000;
 
@@ -12,13 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 app.use("/api", apiRoutes )
+app.use("/", htmlRoutes )
 
-app.get('/notes', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
+// app.get('/notes', (req, res) =>{
+//     res.sendFile(path.join(__dirname, '/public/notes.html'))
+// });
 
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
+// app.get('/', (req, res) =>{
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+// });
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
